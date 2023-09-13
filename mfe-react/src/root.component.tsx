79e89c2de213327755daff43e68cf54be9876abe
Gabriel David Jimenez Mosquera <gabrieljimo@unisabana.edu.co>
2023-09-13 17:07:07 -0500

    Agrega header en mfe-react y su css

--- mfe-react/src/root.component.tsx
+++ mfe-react/src/root.component.tsx
@@ -1,3 +1,27 @@
-export default function Root(props) {
-  return <section>Hola soy el componente en React</section>;
-}
+import React from "react";
+import "./fixedHeader.css";
+
+const FixedHeader: React.FC = () => {
+  return (
+    <header className="fixed-header">
+      <div className="logo">
+        <img src="./imagenes/logo.jpg" />
+      </div>
+      <nav className="menu">
+        <ul>
+          <li>
+            <a href="/">Inicio</a>
+          </li>
+          <li>
+            <a href="/productos">Productos</a>
+          </li>
+          <li>
+            <a href="/contacto">Contacto</a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default FixedHeader;
